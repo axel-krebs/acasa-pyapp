b403de365a3138d9fdd080a51719f3bf6930dd7a
axel-krebs <axelkrebs@online.de>
2023-02-01 13:46:17 +0100

    - Moved web.py to main.py - Provided RequestMapper class - changed Starting routine

--- acasa_web/main.py
+++ acasa_web/main.py
@@ -7,6 +7,7 @@
 import os
 import yaml
 import pathlib
+from quart import Quart, render_template
 
 THIS_PATH = pathlib.Path(__file__).parent
 CONFIG_PATH = "{}{}{}".format(THIS_PATH.resolve(), os.sep, "config.yml")
@@ -20,29 +21,61 @@ with open(CONFIG_PATH, mode = "r", encoding = "UTF-8") as openfile:
 # Provide a means to initialize the database and web server;
 # Things don't get started here.. Goal is to initialize properly!
 # modularized because of invocation from __init__.py, s.th.
-def _init_controls(web_mod,db_mod): 
-    global db_proxy, web_ctl
+def _init_controls(db_mod): 
     init_seq = 0
-    web_ctl = web_mod.create_server(config)
     db_name = config['database']['file_name']
     db_path = os.path.join(os.path.dirname(__file__), db_name)
+    global db_proxy # export
     db_proxy = db_mod.create_proxy(db_path)
-    #web_ctl.create_routes(db_proxy)
+    
     return init_seq
 
-######################################
-# Accessors provided via module import
-######################################
+def create_server(web_config):
+    """
+    Quart initialization; muust be done from main.py because the config.yaml sits there..
+    From scaffold.py: 
+    def __init__(
+        self,
+        import_name: str,
+        static_url_path: Optional[str] = None,
+        static_folder: Optional[str] = "static",
+        static_host: Optional[str] = None,
+        host_matching: bool = False,
+        subdomain_matching: bool = False,
+        template_folder: Optional[str] = "templates",
+        instance_path: Optional[str] = None,
+        instance_relative_config: bool = False,
+        root_path: Optional[str] = None,
+    ) 
 
-def get_web_controller():
-    return web_ctl 
+    Returns:
+        _type_: _description_
+    """
+    static_dir = pathlib.Path("{}{}{}".format(THIS_PATH, os.sep, web_config['static_folder']))
+    template_dir = pathlib.Path("{}{}{}".format(THIS_PATH, os.sep, web_config['template_folder']))
+    web_app_name = web_config['import_name']
+    global web_app # export
+    web_app = Quart(
+        import_name = web_app_name,
+        root_path = THIS_PATH,
+        static_folder = static_dir.resolve(),
+        static_url_path = "/",
+        template_folder = template_dir.resolve()
+    )
 
-def get_db_proxy():
-    return db_proxy
+class RequestMapper():
+    
+    def __init__(self, quart_instance: Quart):
 
-# Associate routes (REST) with DB calls
-def assign_routes():
-    pass
+        @quart_instance.route('/')
+        async def index():
+            return await render_template('index.html')
+
+        @quart_instance.route('/products/')
+        async def list_products():
+            _sql = "SELECT * FROM products"
+            prods = db_proxy.query(_sql)
+            return 'products'
 
 ###################
 # Utility methods #
@@ -76,19 +109,20 @@ def run_test_suite():
 
 def main():
     while True:
-        print("This is the main program. Enter 'z' to leave the program, 't' to run the test suite, 'w' to debug Quart server")
+        print("This is the main program. Enter 'z' to leave the program, 't' to run the test suite, \
+            's' to run the Quart server in DEBUG mode.")
         user_input = input("\n>")
         if user_input == 'z': break
         elif user_input == 't':
             run_test_suite()
         elif user_input == 's':
-            web_ctl.create_routes(db_proxy)
-            web_ctl.debug()
+            create_server(config['web'])
+            RequestMapper(web_app)
+            web_app.run()
 
 if __name__ == "__main__":
-    print("Startup script called on ACASA web directly - checking parameters set..")
+    print("Startup script called on ACASA main directly - checking parameters set..")
     # import in script didn't work!! (Package problem??)
-    import web as web_mod
     import db as db_mod
-    _init_controls(web_mod,db_mod)
+    _init_controls(db_mod)
     main()
